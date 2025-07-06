d94335faf7b1f2f05b2adce3676ac56626ebfceb
Sumit Maradi <123713221+sumit048@users.noreply.github.com>
2025-07-06 11:05:40 +0530

    Styled Streamlit app with red crystal background
    git push
    :wq

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-import streamlit as st
+from flask import Flask, request, render_template, jsonify
+# Alternatively can use Django, FastAPI, or anything similar
 from src.pipeline.prediction_pipeline import CustomData, PredictPipeline
+<<<<<<< HEAD
 
 # Page config
 st.set_page_config(page_title="💎 Diamond Price Predictor", layout="centered")
@@ -74,3 +76,39 @@ with st.container():
             st.error(f"Error: {e}")
 
     st.markdown('</div>', unsafe_allow_html=True)
+=======
+ 
+application = Flask(__name__, static_folder='templates')
+app = application
+ 
+@app.route('/', methods = ['POST', "GET"])
+ 
+def predict_datapoint():
+    if request.method == "GET":
+        return render_template("form.html")
+    else:
+        data = CustomData(
+            carat = float(request.form.get('carat')),
+            depth = float(request.form.get('depth')),
+            table = float(request.form.get("table")),
+            x= float(request.form.get("x")),
+            y = float(request.form.get("y")),
+            z = float(request.form.get("z")),
+            cut = request.form.get("cut"),
+            color = request.form.get("color"),
+            clarity = request.form.get("clarity")
+        )
+    new_data = data.get_data_as_dataframe()
+    predict_pipeline = PredictPipeline()
+    pred = predict_pipeline.predict(new_data)
+ 
+    results = round(pred[0],2)
+ 
+    return render_template("form.html", final_result = results)
+ 
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", debug= True)
+ 
+#http://127.0.0.1:5000/ in browser
+ 
+>>>>>>> 03e8454 (Styled Streamlit app with red crystal background)
