db8b87c15382ffe6c10e44ddbc1e45848c8735e6
Sumit Maradi <123713221+sumit048@users.noreply.github.com>
2025-10-28 14:32:50 +0530

    Remove main execution block from app.py

--- app.py
+++ app.py
@@ -105,7 +105,4 @@ def predict_datapoint():
  
     return render_template("form.html", final_result = results)
  
-if __name__ == "__main__":
-    app.run(host = "0.0.0.0", debug= True)
- 
 #http://127.0.0.1:5000/ in browser
